91d0cf83546f19975101e6201f742927390e94ff
atonlife <atonlife@gmail.com>
2022-01-14 22:18:58 +0300

    #2 Getting Issues by JQL

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # aTeam-WorkLog
 Golang sandbox application for monitoring Team Work Log in Jira
+
+Exemple **`secret.config.json`**
+```json
+{
+    "hostname": "http://<JIRA_HOSTNAME>",
+    "username": "<USERNAME>",
+    "password": "<PASSWORD>",
+    "jql": "<JQL>"
+}
+```
+
+Exemple **JQL** for one day:
+```sql
+worklogAuthor = <AUTHOR>
+	AND
+worklogDate >= <LAST_WORKING_DAY>
+	AND
+worklogDate <= <LAST_WORKING_DAY>
+```
--- src/main.go
+++ src/main.go
@@ -11,51 +11,120 @@ import (
 )
 
 const (
-	cfgPath    = "./etc"
-	cfgConnect = "secret.connect.json"
+	configPath    = "./etc"
+	configConnect = "secret.config.json"
 
 	urlRest             = "/rest/api/2"
 	urlMethodCreatemeta = "/issue/createmeta"
+	urlMethodSearch     = "/search"
 )
 
-
-type CfgConnectT struct {
-	Server   string `json:"server"`
-	User     string `json:"user"`
-	Password string `json:"password"`
+type ConfigT struct {
+	Hostname string `json:"hostname,omitempty"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	JQL      string `json:"jql,omitempty"`
 }
 
+type JiraSearchIssueT struct {
+	MaxResults int           `json:"maxResults,omitempty"`
+	Total      int           `json:"total,omitempty"`
+	Issues     []JiraIssuesT `json:"issues,omitempty"`
+}
+
+type JiraIssuesT struct {
+	Key string `json:"key,omitempty"`
+}
+
+// MAIN ------------------------------------------------------------------------
 
 func main() {
-	connect := getCfgConnect(cfgConnect)
-	response := getResponse(connect, urlMethodCreatemeta)
-	readResponse(response)
-}
+	config := getconfigConnect(configConnect)
 
-func getCfgConnect(filename string) CfgConnectT {
-	var connect CfgConnectT
+	issues := jiraSearchIssue(config)
 
-	filesystem := os.DirFS(cfgPath)
-	connectJSON, err := fs.ReadFile(filesystem, filename)
-	if err != nil {
-		log.Fatalf("Error in JSON: %s", err.Error())
+	var totalWorklog int
+	for _, issue := range issues {
+		totalWorklog += jiraIssueGetWorklog(issue.Key)
 	}
 
-	json.Unmarshal(connectJSON, &connect)
+	fmt.Printf("Total Worklog: %d\n", totalWorklog)
+}
+
+// CONFIG ----------------------------------------------------------------------
+
+func getconfigConnect(filename string) ConfigT {
+	filesystem := os.DirFS(configPath)
+	file, err := fs.ReadFile(filesystem, filename)
+	if err != nil {
+		log.Fatalf("Error in file: '%s'", err.Error())
+	}
+
+	var connect ConfigT
+	err = json.Unmarshal(file, &connect)
+	if err != nil {
+		log.Fatalf("Error in unmarshal '%s'", err.Error())
+	}
 
 	return connect
 }
 
-func getResponse(connect CfgConnectT, method string) *http.Response {
-	url := connect.Server + urlRest + method
+// JIRA ------------------------------------------------------------------------
+
+// JIRA ISSUE
+
+func jiraSearchIssue(config ConfigT) []JiraIssuesT {
+	url := urlMethodSearch + "?jql=" + config.JQL
+	response := jiraRestGet(config, url)
+
+	var search JiraSearchIssueT
+	err := json.Unmarshal(response, &search)
+	if err != nil {
+		log.Fatalf("Error in unmarshal '%s'", err.Error())
+	}
+
+	// Max Result by default is 50 items
+	if search.Total > search.MaxResults {
+		log.Fatalf("Total items '%d' more max result '%d'", search.Total, search.MaxResults)
+	}
+
+	return search.Issues
+}
+
+// JIRA ISSUE WORKLOG
+
+func jiraIssueGetWorklog(issue string) int {
+	//TODO: Get sum issue worklog by period and author (get_issue_worklog)
+	var totalWorklog int
+
+	return totalWorklog
+}
+
+// JIRA REST -------------------------------------------------------------------
+
+func jiraRestGet(config ConfigT, urlRequest string) []byte {
+	url := config.Hostname + urlRest + urlRequest
+	log.Printf("Request URL '%s'", url)
+	response := sendGet(url, config.Username, config.Password)
+
+	log.Printf("Response Status '%d'", response.StatusCode)
+	if response.StatusCode != http.StatusOK {
+		log.Fatalf("Jira Response Status isn't OK")
+	}
+
+	return readBody(response)
+}
+
+// REST ------------------------------------------------------------------------
+
+func sendGet(url, user, password string) *http.Response {
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	request.SetBasicAuth(connect.User, connect.Password)
+	request.SetBasicAuth(user, password)
 
 	client := &http.Client{}
-
 	response, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
@@ -64,14 +133,12 @@ func getResponse(connect CfgConnectT, method string) *http.Response {
 	return response
 }
 
-func readResponse(response *http.Response) {
+func readBody(response *http.Response) []byte {
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
 
-	//TODO: DEBUG
-	fmt.Println(response.StatusCode == http.StatusOK)
-	fmt.Println(body != nil)
+	return body
 }
